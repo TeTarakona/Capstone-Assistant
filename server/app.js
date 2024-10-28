4dfca24a9da7a852d1b161e8081bc88a60683b56
tarakona <michaelrosen44@gmail.com>
2024-10-29 00:15:59 +1300

    Created a POST request that uses SQL to insert the body attribute of the request object into the database

--- server/app.js
+++ server/app.js
@@ -1,14 +1,27 @@
 require("dotenv").config();
 const express = require("express")
 const app = express();
-const cors = require("cors")
+const cors = require("cors");
+const { Pool } = require("pg");
 
 //Middleware
 app.use(cors());
-app.use(express.json()); //req.body
+app.use(express.json()); //This line makes it easier to access information in the request
 
 app.get("/", async (req, res) => {
     res.json({message: "Welcome to Alec's capstone project"});
 });
+
+
+app.post("/todo", async (req, res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await Pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", [description])
+        
+        res.json(newTodo.rows[0])
+    } catch (error) {
+        console.error(err.message)
+    }
+})
 
 module.exports = app;
