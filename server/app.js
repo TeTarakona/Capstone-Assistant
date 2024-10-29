ea9a8fa7a57aa63fddf0f05127840fe17365f097
tarakona <michaelrosen44@gmail.com>
2024-10-29 13:33:45 +1300

    Upgraded DB version. Installed dotenv correctly. Fixed routes to not be in each other's scopes

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const express = require("express")
+const express = require("express");
 const app = express();
 const cors = require("cors");
 const { Pool } = require("pg");
@@ -9,30 +9,33 @@ app.use(cors());
 app.use(express.json()); //This line makes it easier to access information in the request
 
 app.get("/", async (req, res) => {
-    res.json({message: "Welcome to Alec's capstone project"});
+  res.json({ message: "Welcome to Alec's capstone project" });
 });
 
 //This route adds a task to the todo list
 app.post("/todo", async (req, res) => {
-    try {
-        const { description } = req.body; //Here, description is the field name in the database.
-        const newTodo = await Pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", [description]) //A variable is created that is assigned the result of the awaited query. Standard SQL, with the returning statement at the end being the code that sets the variable to whatever data was inserted into the database.
-        
-        res.json(newTodo.rows[0]) //That variable then gets used to set the server response object. Rows[0] being the latest thing added to the database, IE the newly inserted task.
+  try {
+    const { description } = req.body; //Here, description is the field name in the database.
+    const newTodo = await Pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    ); //A variable is created that is assigned the result of the awaited query. Standard SQL, with the returning statement at the end being the code that sets the variable to whatever data was inserted into the database.
 
-        //Either the above happens or everything fails and gets caught below.
-    } catch (error) {
-        console.error(err.message)
-    }
+    res.json(newTodo.rows[0]); //That variable then gets used to set the server response object. Rows[0] being the latest thing added to the database, IE the newly inserted task.
 
-    app.get("/todo", async (req, res) => {
-        try {
-            const todos = await Pool.query("SELECT * FROM todo")
-            res.json(todos)
-        } catch (error) {
-            console.error(err.message)
-        }
-    })
-})
+    //Either the above happens or everything fails and gets caught below.
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+app.get("/todo", async (req, res) => {
+  try {
+    const todos = await Pool.query("SELECT * FROM todo");
+    res.json(todos);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 module.exports = app;
