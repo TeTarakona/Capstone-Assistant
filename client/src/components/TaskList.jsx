6b5ba8c6380d6a7c3630fdade72812ddb2755707
tarakona <michaelrosen44@gmail.com>
2024-10-31 23:30:58 +1300

    Added functionality to the delete button in the task list (And it works c: )

--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -3,6 +3,17 @@ import React, { useEffect, useState } from "react";
 const Tasklist = () => {
   const [tasks, setTasks] = useState([]);
 
+  const deleteTask = async (id) => {
+    try {
+        const deleteTask = await fetch(`http://localhost:5001/todo/${id}`, {
+            method: "DELETE"
+        })
+        setTasks(tasks.filter(task => task.todo_id !== id))
+    } catch (error) {
+        console.error(error.message)
+    }
+}
+
   const getTasks = async () => {
     try {
       const response = await fetch("http://localhost:5001/todo");
@@ -29,13 +40,13 @@ const Tasklist = () => {
           </tr>
         </thead>
         <tbody>
-        {tasks.map(task => (
-            <tr key={task.id}>
-                <td>{task.description}</td>
-                <td>Edit</td>
-                <td>Delete</td>
+          {tasks.map((task) => (
+            <tr key={task.todo_id}>
+              <td>{task.description}</td>
+              <td>Edit</td>
+              <button onClick={() => deleteTask(task.todo_id)}>Delete</button>
             </tr>
-        ))}
+          ))}
         </tbody>
       </table>
     </>
