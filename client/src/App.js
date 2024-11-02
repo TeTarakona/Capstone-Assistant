4d9d3fb32e130bd2aeecec3f8512d899a2e09b43
tarakona <michaelrosen44@gmail.com>
2024-11-02 20:16:58 +1300

    Created a route for the Calendar component

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import './App.css';
 
 import TaskListPage from "./components/TaskListPage";
+import CalendarComponent from "./components/CalendarComponent"
 
 function App() {
   return (
     <>
     <Routes>
       <Route path="/tasklist" element={<TaskListPage />} />
+      <Route path="/calendar" element={<CalendarComponent />} />
     </Routes>
     </>
   );
