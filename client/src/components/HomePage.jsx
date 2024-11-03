45a421f38a2f644fc0df0e3d6353084e015710ee
tarakona <michaelrosen44@gmail.com>
2024-11-03 23:10:39 +1300

    Styled tagline

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -6,7 +6,7 @@ const HomePage = () => {
         <>
         <div className="pageContainer">
         <h1 className="heading" >Prio</h1>
-        <p>The personal assistant app</p>
+        <p className="tagline">The personal assistant app</p>
         <article>
             <h2>Welcome to Prio</h2>
             <p>Prio is a personal assistant application that aims to combine useful time management tools into a single cloud based platform that can be accessed from any browser.</p>
