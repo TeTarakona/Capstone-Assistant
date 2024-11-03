cf9ad74d43b6cd0f3641fd156be24847c5db50d4
tarakona <michaelrosen44@gmail.com>
2024-11-04 00:58:23 +1300

    Attempt to make the navigation menu look how I want it to. It will need more attention at a later date

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,28 +1,12 @@
 import { Link } from "react-router-dom"
 
+import "./Nav.css"
+
 const Nav = () => {
   return (
     <>
       <div class="pos-f-t">
-        <div class="collapse" id="navbarToggleExternalContent">
-          <div class="bg-dark p-4">
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/notepad">Notepad</Link>
-                </li>
-                <li>
-                    <Link to="/tasklist">Task List</Link>
-                </li>
-                <li>
-                    <Link to="/calendar">Calendar</Link>
-                </li>
-            </ul>
-          </div>
-        </div>
-        <nav class="navbar navbar-dark bg-dark">
+        <nav class="navbar navbar-dark bg-dark float-right">
           <button
             class="navbar-toggler"
             type="button"
@@ -35,6 +19,24 @@ const Nav = () => {
             <span class="navbar-toggler-icon"></span>
           </button>
         </nav>
+        <div class="collapse" id="navbarToggleExternalContent">
+          <div className="menuBackground">
+            <ul>
+                <li>
+                    <Link to="/" className="navLink" >Home</Link>
+                </li>
+                <li>
+                    <Link to="/notepad" className="navLink" >Notepad</Link>
+                </li>
+                <li>
+                    <Link to="/tasklist" className="navLink" >Task List</Link>
+                </li>
+                <li>
+                    <Link to="/calendar" className="navLink" >Calendar</Link>
+                </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </>
   );
